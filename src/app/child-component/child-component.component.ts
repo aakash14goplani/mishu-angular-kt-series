3090ef1e8e5ac743c5e443904475d52ea57b9989
aakash14goplani <goplaniaakash14@gmail.com>
2020-11-04 17:31:25 +0530

    completed life cycle hook

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -1,23 +1,47 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnChanges, DoCheck, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-child-component',
   templateUrl: './child-component.component.html',
   styleUrls: ['./child-component.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges, DoCheck, AfterViewInit, AfterViewChecked, OnDestroy {
 
   @Input() messageReceiver;
   @Output() messageFromChild = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {
+    console.log('sss constructor fired...');
+  }
 
   ngOnInit(): void {
+    console.log('sss ngOnInit fired...');
+  }
+
+  ngOnChanges(): void {
+    console.log('sss ngOnChanges fired...');
+  }
+
+  ngDoCheck(): void {
+    console.log('sss ngDoCheck fired...');
+  }
+
+  ngAfterViewInit(): void{
+    console.log('sss ngAfterViewInit fired...');
+  }
+
+  ngAfterViewChecked(): void{
+    console.log('sss ngAfterViewChecked fired...');
   }
 
   clicked(event: any): void {
     console.log('Event fired with data: ', event);
     this.messageFromChild.emit('Haan Khal liya...');
+  }
+
+  ngOnDestroy(): void {
+    console.log('sss ngOnDestroy fired...');
   }
 
 }
